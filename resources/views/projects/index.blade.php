d4d8bd1fe9d34234c9de5cb806d4ada55b919e24
ShinyQ_11 <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2020-06-05 18:31:56 +0700

    ADD | Part 9 : Setup Home Design

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,15 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="flex items-center">
-    <h1 class="mr-auto">BirdBoard</h1>
+  <div class="flex items-center mb-3">
     <a href="/projects/create">New Project</a>
   </div>
-  <ul>
-    @forelse($projects as $project)
-      <li><a href="{{ $project->path() }}">{{ $project->title }}</a></li>
-    @empty
-      <li>No Data Yet</li>
-    @endforelse
-  </ul>
+
+  <div class="flex">
+      @forelse($projects as $project)
+        <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height:200px">
+            <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+            <div class="text-grey">
+              {{ Illuminate\Support\Str::limit($project->description, 100) }}
+            </div>
+        </div>
+      @empty
+        <div>No Project Yet</div>
+      @endforelse
+  </div>
 @endsection
