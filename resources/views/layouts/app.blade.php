16c553bb87e5be12f2f174ea0b9f838434236a2e
ShinyQ_11 <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2020-06-05 21:27:42 +0700

    ADD | Part 10, Setup navbar and make landing page more beautiful :)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -19,47 +19,47 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body class="bg-grey-light" style="margin:0!important">
+<body class="bg-grey-light">
     <div id="app">
         <nav class="bg-white">
             <div class="container mx-auto">
-              <div class="flex justify-between items-center py-2">
-                  <a class="navbar-brand" href="{{ url('/') }}">
-                      {{ config('app.name', 'Laravel') }}
-                  </a>
+                <div class="flex justify-between items-center py-2">
+                  <h1>
+                      <a class="navbar-brand" href="{{ url('/') }}">
+                          <img src="/images/logo.svg" alt="Birdboard">
+                      </a>
+                   </h1>
 
-                  <div>
-                      <!-- Right Side Of Navbar -->
-                      <ul class="navbar-nav ml-auto">
-                          <!-- Authentication Links -->
-                          @guest
-                              <li class="nav-item">
-                                  <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                              </li>
-                              @if (Route::has('register'))
-                                  <li class="nav-item">
-                                      <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                  </li>
-                              @endif
-                          @else
-                              <li class="nav-item dropdown">
-                                  <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                      {{ Auth::user()->name }} <span class="caret"></span>
-                                  </a>
+                   <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
 
-                                  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                      <a class="dropdown-item" href="{{ route('logout') }}"
-                                         onclick="event.preventDefault();
-                                                       document.getElementById('logout-form').submit();">
-                                          {{ __('Logout') }}
-                                      </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
 
-                                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                          @csrf
-                                      </form>
-                                  </div>
-                              </li>
-                          @endguest
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
                       </ul>
                   </div>
                 </div>
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,20 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="flex items-center mb-3">
-    <a href="/projects/create">New Project</a>
+<header class="flex items-center mb-3 py-4">
+  <div class="flex justify-between items-center w-full">
+    <h2 class="text-grey text-sm font-normal">My Project</h2>
+    <a href="/projects/create" class="button">New Project</a>
   </div>
+</header>
 
-  <div class="flex">
-      @forelse($projects as $project)
-        <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height:200px">
-            <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+<main class="lg:flex lg:flex-wrap -mx-3">
+    @forelse($projects as $project)
+      <div class="lg:w-1/3 px-3 pb-6">
+        <div class="bg-white p-5 rounded-lg shadow" style="height:200px">
+            <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-light pl-4">
+                <a class="text-black no-underline" href="{{ $project->path() }}">{{ $project->title }}</a>
+            </h3>
             <div class="text-grey">
               {{ Illuminate\Support\Str::limit($project->description, 100) }}
             </div>
         </div>
-      @empty
-        <div>No Project Yet</div>
-      @endforelse
-  </div>
+      </div>
+    @empty
+      <div>No Project Yet</div>
+    @endforelse
+</main>
 @endsection
