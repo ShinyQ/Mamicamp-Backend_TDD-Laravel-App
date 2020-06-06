a54974657f56c82ed08bb4ae8d59016d39cb364d
ShinyQ_11 <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2020-06-06 19:51:18 +0700

    ADD | Part 30 : Setup Project Invitations and their Testing

--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
 
 class ProjectTest extends TestCase
 {
@@ -34,5 +35,14 @@ class ProjectTest extends TestCase
 
         $this->assertCount(1, $project->tasks);
         $this->assertTrue($project->tasks->contains($task));
+    }
+
+    /** @test */
+    function it_can_invite_a_user()
+    {
+        $project = factory('App\Project')->create();
+        $project->invite($user = factory(User::class)->create());
+
+        $this->assertTrue($project->members->contains($user));
     }
 }
