c9b69a2fcbd41f1590fd35134634e4f0848a1835
ShinyQ_11 <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2020-06-06 17:46:00 +0700

    ADD | Part 25 : Subject For The Activity and The Testing

--- app/Task.php
+++ app/Task.php
@@ -17,15 +17,13 @@ class Task extends Model
     public function complete()
     {
         $this->update(['completed' => true]);
-
-        $this->project->recordActivity('completed_task');
+        $this->recordActivity('completed_task');
     }
 
     public function incomplete()
     {
         $this->update(['completed' => false]);
-
-        $this->project->recordActivity('incompleted_task');
+        $this->recordActivity('incompleted_task');
     }
 
     public function project()
@@ -36,5 +34,18 @@ class Task extends Model
     public function path()
     {
         return "/projects/{$this->project->id}/tasks/{$this->id}";
+    }
+
+    public function recordActivity($description)
+    {
+        $this->activity()->create([
+            'project_id' => $this->project_id,
+            'description' => $description
+        ]);
+    }
+
+    public function activity()
+    {
+        return $this->morphMany(Activity::class, 'subject')->latest();
     }
 }
