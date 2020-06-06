eb6811b469f72ceaac6eebf37f9ab4b9a07b53e9
ShinyQ_11 <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2020-06-06 22:07:52 +0700

    ADD | Part 36, 37 and 38, Setup Tailwind Dark And Light Theme, Setup Switch Theme For The Projects, Refactoring All UI View In The Projects

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -1,10 +1,11 @@
 @extends ('layouts.app')
 
 @section('content')
-    <div class="lg:w-1/2 lg:mx-auto bg-white p-6 md:py-12 md:px-16 rounded shadow">
+    <div class="lg:w-1/2 lg:mx-auto bg-card p-6 md:py-12 md:px-16 rounded shadow">
         <h1 class="text-2xl font-normal mb-10 text-center">
             Edit Your Project
         </h1>
+
         <form
                 method="POST"
                 action="{{ $project->path() }}"
