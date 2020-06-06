9c31b2a81117fb5a9a2bc7b8548f78fdfcf3e803
ShinyQ_11 <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2020-06-06 23:03:49 +0700

    ADD | Part 42, Adding Data Project From Modal

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/projects';
 
     /**
      * Create a new controller instance.
